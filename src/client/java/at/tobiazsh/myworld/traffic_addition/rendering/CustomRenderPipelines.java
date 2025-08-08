874f9c3f55af05ea33dd30c86d19884028e37461
Tobias <51285655+tobiazsh@users.noreply.github.com>
2025-08-09 00:59:17 +0200

    custom render pipeline as an attempt to fix flickering with text in the new version

---
+++ src/client/java/at/tobiazsh/myworld/traffic_addition/rendering/CustomRenderPipelines.java
@@ -0,0 +1,23 @@
+package at.tobiazsh.myworld.traffic_addition.rendering;
+
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import net.minecraft.client.gl.RenderPipelines;
+
+import static net.minecraft.client.gl.RenderPipelines.FOG_SNIPPET;
+import static net.minecraft.client.gl.RenderPipelines.TEXT_SNIPPET;
+
+public class CustomRenderPipelines {
+
+    public static final RenderPipeline RENDERTYPE_CUSTOM_TEXT_INTENSITY;
+
+    static {
+        RENDERTYPE_CUSTOM_TEXT_INTENSITY = RenderPipelines.register(RenderPipeline.builder(TEXT_SNIPPET, FOG_SNIPPET)
+                .withLocation("pipeline/text_intensity")
+                .withVertexShader("core/rendertype_text_intensity")
+                .withFragmentShader("core/rendertype_text_intensity")
+                .withSampler("Sampler0")
+                .withSampler("Sampler2")
+                .withDepthBias(-1.0F, -10.0F).build());
+
+    }
+}
