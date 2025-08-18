2652e6ef3b26a8e1eed9a01beedb85de7252fa53
Tobias <51285655+tobiazsh@users.noreply.github.com>
2025-08-18 15:50:54 +0200

    change location

--- src/main/java/at/tobiazsh/myworld/traffic_addition/block_entities/TriangularSignBlockEntity.java
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/block_entities/TriangularSignBlockEntity.java
@@ -1,6 +1,6 @@
 package at.tobiazsh.myworld.traffic_addition.block_entities;
 
-import at.tobiazsh.myworld.traffic_addition.ModVars;
+import at.tobiazsh.myworld.traffic_addition.blocks.SignBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 
@@ -9,7 +9,7 @@ import static at.tobiazsh.myworld.traffic_addition.ModBlockEntities.TRIANGULAR_S
 public class TriangularSignBlockEntity extends SignBlockEntity {
 
     public TriangularSignBlockEntity(BlockPos pos, BlockState state) {
-        super(TRIANGULAR_SIGN_BLOCK_ENTITY, pos, state, ModVars.SIGN_SELECTION_TYPE.TRIANGULAR_UPSIDE_UP, "textures/sign_pngs/aut/triangular/other_danger.png");
+        super(TRIANGULAR_SIGN_BLOCK_ENTITY, pos, state, SignBlock.SIGN_SHAPE.TRIANGULAR_UPSIDE_UP, "textures/sign_pngs/aut/triangular/other_danger.png");
     }
 
 }
