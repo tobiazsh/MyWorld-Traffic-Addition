010ab810033708d12fe049c43f85b86f0da5c874
Tobias <51285655+tobiazsh@users.noreply.github.com>
2025-08-18 15:41:22 +0200

    remove a bunch of unused imports

--- src/main/java/at/tobiazsh/myworld/traffic_addition/blocks/TriangularSignBlock.java
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/blocks/TriangularSignBlock.java
@@ -1,18 +1,11 @@
 package at.tobiazsh.myworld.traffic_addition.blocks;
 
-import at.tobiazsh.myworld.traffic_addition.ModVars;
-import at.tobiazsh.myworld.traffic_addition.MyWorldTrafficAddition;
 import at.tobiazsh.myworld.traffic_addition.block_entities.TriangularSignBlockEntity;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class TriangularSignBlock extends SignBlock {
