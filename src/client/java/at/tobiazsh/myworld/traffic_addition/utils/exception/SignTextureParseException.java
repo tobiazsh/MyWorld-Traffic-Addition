7c5507d77ad4ab0e971e833b3c3c85d130bb24a5
Tobias <51285655+tobiazsh@users.noreply.github.com>
2025-09-03 13:22:17 +0200

    Add exception for when MWTA couldn't parse all the textures from textures.json

---
+++ src/client/java/at/tobiazsh/myworld/traffic_addition/utils/exception/SignTextureParseException.java
@@ -0,0 +1,11 @@
+package at.tobiazsh.myworld.traffic_addition.utils.exception;
+
+public class SignTextureParseException extends RuntimeException {
+    public SignTextureParseException(String message) {
+        super(message);
+    }
+
+    public SignTextureParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
