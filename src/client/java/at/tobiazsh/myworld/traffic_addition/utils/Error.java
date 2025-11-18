68cf3a1f023d370b72baf0ead66d686fea1814f1
Tobiazsh <51285655+tobiazsh@users.noreply.github.com>
2025-11-18 14:28:55 +0100

    Make Error byte-able

--- src/client/java/at/tobiazsh/myworld/traffic_addition/utils/Error.java
+++ src/client/java/at/tobiazsh/myworld/traffic_addition/utils/Error.java
@@ -1,5 +1,7 @@
 package at.tobiazsh.myworld.traffic_addition.utils;
 
+import java.nio.ByteBuffer;
+
 /**
  * Simple error class to represent errors with title and message
  */
@@ -37,5 +39,39 @@ public class Error {
 
     public String getMessage() {
         return message;
+    }
+
+    public byte[] toBytes() {
+        ByteBuffer buffer = ByteBuffer.allocate(4 + title.length() + 4 + message.length() + 1);
+        buffer.putInt(title.length());
+        buffer.put(title.getBytes());
+
+        buffer.putInt(message.length());
+        buffer.put(message.getBytes());
+
+        buffer.put((byte) (isHandled ? 1 : 0));
+        return buffer.array();
+    }
+
+    public static Error fromBytes(byte[] data) {
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+
+        int titleLength = buffer.getInt();
+        byte[] titleBytes = new byte[titleLength];
+        buffer.get(titleBytes);
+        String title = new String(titleBytes);
+
+        int messageLength = buffer.getInt();
+        byte[] messageBytes = new byte[messageLength];
+        buffer.get(messageBytes);
+        String message = new String(messageBytes);
+
+        boolean isHandled = buffer.get() == 1;
+
+        Error error = new Error(title, message);
+        if (isHandled) {
+            error.handled();
+        }
+        return error;
     }
 }
