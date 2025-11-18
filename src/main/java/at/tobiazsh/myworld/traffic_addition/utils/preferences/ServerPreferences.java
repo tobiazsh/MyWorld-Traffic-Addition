59f9529764b68957cdd1c5379e572378e21754a0
Tobiazsh <51285655+tobiazsh@users.noreply.github.com>
2025-11-18 13:59:06 +0100

    add maximum thumbnail upload size

--- src/main/java/at/tobiazsh/myworld/traffic_addition/utils/preferences/ServerPreferences.java
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/utils/preferences/ServerPreferences.java
@@ -5,12 +5,19 @@ public class ServerPreferences {
     public static Preference generalServerPreferences = new Preference("myworld_traffic_addition/server_config.json");
 
     public static long maximumImageUploadSize = 1024 * 1024 * 5; // 5MB; Default
-    public static long maximumImageUploadSizeDefault = 1024 * 1024 * 5; // 5MB; Default
+    public static final long maximumImageUploadSizeDefault = 1024 * 1024 * 5; // 5MB; Default
+
+    public static long maximumThumbnailUploadSize = 1024 * 512; // 512KB; Default
+    public static final long maximumThumbnailUploadSizeDefault = 1024 * 512; // 512KB; Default
 
     public static void loadPreferences() {
         // Load server preferences
         maximumImageUploadSize = generalServerPreferences.getLong("maximumImageUploadSize");
         if (maximumImageUploadSize == Preference.INVALID_LONG)
             maximumImageUploadSize = maximumImageUploadSizeDefault; // Fallback to default
+
+        maximumThumbnailUploadSize = generalServerPreferences.getLong("maximumThumbnailUploadSize");
+        if (maximumThumbnailUploadSize == Preference.INVALID_LONG)
+            maximumThumbnailUploadSize = maximumThumbnailUploadSizeDefault; // Fallback to default
     }
 }
