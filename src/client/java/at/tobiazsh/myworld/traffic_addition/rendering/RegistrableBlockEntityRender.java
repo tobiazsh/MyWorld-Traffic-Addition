9eb0501dc0c26fe0c74f4c3912a244c88063beb1
Tobiazsh <51285655+tobiazsh@users.noreply.github.com>
2025-12-02 21:54:17 +0100

    Update class to 1.21.10

--- src/client/java/at/tobiazsh/myworld/traffic_addition/rendering/RegistrableBlockEntityRender.java
+++ src/client/java/at/tobiazsh/myworld/traffic_addition/rendering/RegistrableBlockEntityRender.java
@@ -4,23 +4,24 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.block.entity.state.BlockEntityRenderState;
 
 import java.util.List;
 
-public class RegistrableBlockEntityRender <T extends BlockEntity> {
+public class RegistrableBlockEntityRender <T extends BlockEntity, S extends BlockEntityRenderState> {
     public BlockEntityType<T> blockEntityType;
-    public BlockEntityRendererFactory<T> blockEntityRenderer;
+    public BlockEntityRendererFactory<T ,S> blockEntityRenderer;
 
-    public RegistrableBlockEntityRender(BlockEntityType<T> blockEntityType, BlockEntityRendererFactory<T> blockEntityRenderer) {
+    public RegistrableBlockEntityRender(BlockEntityType<T> blockEntityType, BlockEntityRendererFactory<T, S> blockEntityRenderer) {
         this.blockEntityType = blockEntityType;
         this.blockEntityRenderer = blockEntityRenderer;
     }
 
-    public static <T extends BlockEntity> void RegisterBlockEntityRenderer(RegistrableBlockEntityRender<T> registrableBlockEntityRender) {
+    public static <T extends BlockEntity, S extends BlockEntityRenderState> void RegisterBlockEntityRenderer(RegistrableBlockEntityRender<T, S> registrableBlockEntityRender) {
         BlockEntityRendererFactories.register(registrableBlockEntityRender.blockEntityType, registrableBlockEntityRender.blockEntityRenderer);
     }
 
-    public static void bulkRegisterBlockEntityRenderers(List<RegistrableBlockEntityRender<? extends BlockEntity>> blockEntityRenderers) {
+    public static void bulkRegisterBlockEntityRenderers(List<RegistrableBlockEntityRender<? extends BlockEntity, ? extends BlockEntityRenderState>> blockEntityRenderers) {
         blockEntityRenderers.forEach(RegistrableBlockEntityRender::RegisterBlockEntityRenderer);
     }
 }
