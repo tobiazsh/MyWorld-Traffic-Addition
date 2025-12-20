355115a008273a2b1a62a4df5b928ac1ab31a7c1
Tobiazsh <51285655+tobiazsh@users.noreply.github.com>
2025-12-20 15:23:17 +0100

    convert list to hashset to not contain any dups

--- src/main/java/at/tobiazsh/myworld/traffic_addition/block_entities/CustomizableSignBlockEntity.java
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/block_entities/CustomizableSignBlockEntity.java
@@ -64,7 +64,7 @@ public class CustomizableSignBlockEntity extends BlockEntity {
     private int width = 1;
 
     public boolean editAllowed = true;
-    public List<UUID> editExceptionUUIDs = new ArrayList<>();
+    public Set<UUID> editExceptionUUIDs = new HashSet<>();
 
     // Texture variables
     // These variables are temporary and deleted after the program is closed. It is solely used to reduce the amount of operations it would take to update the textures each render. If it'd be this way, it can easily slow down the game by a lot if there are lots of these signs present.
