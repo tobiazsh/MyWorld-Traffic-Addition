8c34adf197c1ac9f8f4261257a13aadf90421354
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-01-25 14:36:01 +0100

    Easily Store Color from any format

---
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/utils/Color.java
@@ -0,0 +1,62 @@
+package at.tobiazsh.myworld.traffic_addition.utils;
+
+/**
+ * Represents a color with alpha, red, green, and blue components.
+ * @param a Alpha
+ * @param r Red
+ * @param g Green
+ * @param b Blue
+ */
+public record Color(int a, int r, int g, int b) {
+
+    public Color {
+        if (isInvalid(a) || isInvalid(r) || isInvalid(g) || isInvalid(b)) {
+            throw new IllegalArgumentException("Color values must be between 0 and 255");
+        }
+    }
+
+    /**
+     * Creates a new Color instance with full opacity
+     * @param r The red value
+     * @param g The green value
+     * @param b The blue value
+     */
+    public Color(int r, int g, int b) {
+        this(255, r, g, b);
+    }
+
+    /**
+     * Creates a new Color instance from a single ARGB integer
+     * @param rgba The RGBA integer
+     */
+    public Color(int rgba, boolean hasAlpha) {
+        this(
+            hasAlpha ? (rgba >> 24) & 0xFF : 255,
+            (rgba >> 16) & 0xFF,
+            (rgba >> 8)& 0xFF,
+            rgba & 0xFF
+        );
+    }
+
+    private static boolean isInvalid(int colorVal) {
+        return colorVal < 0 || colorVal > 255;
+    }
+
+    /**
+     * Converts an ARGB integer to RGBA format
+     * @param rgba The ARGB integer
+     * @return The RGBA integer
+     */
+    public static int ARGB2RGBA(int rgba) {
+        return ((rgba & 0x00FFFFFF) << 8) | ((rgba >> 24) & 0xFF);
+    }
+
+    /**
+     * Converts an RGBA integer to ARGB format
+     * @param rgba The RGBA integer
+     * @return The ARGB integer
+     */
+    public static int RGBA2ARGB(int rgba) {
+        return ((rgba & 0xFFFFFF00) >> 8) | ((rgba & 0xFF) << 24);
+    }
+}
