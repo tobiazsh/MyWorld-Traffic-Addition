cdbe62cd993e7466feea33bd208e18b50dfc6dc5
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-07-17 16:32:41 +0200

    High-level abstractions for loading and saving a preference class to a TOML file

--- src/main/java/at/tobiazsh/myworld/traffic_addition/preference/PreferenceLoader.java
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/preference/PreferenceLoader.java
@@ -1,56 +1,99 @@
 package at.tobiazsh.myworld.traffic_addition.preference;
 
-import at.tobiazsh.myworld.traffic_addition.exception.PreferenceReadException;
-import at.tobiazsh.myworld.traffic_addition.exception.PreferenceWriteException;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.toml.TomlMapper;
+import at.tobiazsh.myworld.traffic_addition.preference.serialization.PreferenceSerializer;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class PreferenceLoader {
 
-    private static TomlMapper createMapper() {
-        TomlMapper mapper = new TomlMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        return mapper;
-    }
-
     /**
-     * Loads a TOML file from storage and tries to deserialize to any class of type PreferenceHierarchy.
-     * @param file The file to deserialize
-     * @return The serialized PreferenceHierarchy class
-     * @param <P> The type of PreferenceHierarchy class
-     * @throws PreferenceReadException If the file could not be read or deserialized
+     * Tries to load a preference class from TOML in the specified location, otherwise resorts to default value and
+     * invokes callback.
+     *
+     * @param file The file to load the TOML from.
+     * @param clazz The class to deserializes the TOML into.
+     * @param defaultValueSupplier The value to return if an exception is caught.
+     * @param onFallbackCallback Message supplier containing the error message. Can be null if logging is irrelevant.
+     * @return The deserialized preference object.
+     * @param <T> The type of preference class.
      */
-    public static <P extends PreferenceHierarchy> P load(File file, Class<P> preferenceClass) throws PreferenceReadException {
-        TomlMapper mapper = createMapper();
-
+    public static <T extends PreferenceHierarchy> T loadPreferenceFromFileOrDefault(
+            File file,
+            T clazz,
+            Supplier<T> defaultValueSupplier,
+            @Nullable Consumer<String> onFallbackCallback
+    ) {
         try {
-            return mapper.readValue(file, preferenceClass);
+            return loadPreferencesFromFile(file, clazz);
         } catch (IOException e) {
-            throw new PreferenceReadException(
-                    String.format("Failed to read preferences from %s", file.getAbsolutePath()),
-                    e
-            );
+            if (onFallbackCallback != null) {
+                onFallbackCallback.accept(
+                        "Failed to load preferences from file: "
+                                + file.getAbsolutePath() +
+                                ". Falling back to default preferences."
+                );
+            }
+
+            return defaultValueSupplier.get();
         }
     }
 
     /**
-     * Serializes and saves a class of type PreferenceHierarchy to a file in the TOML format.
-     * @param file The TOML file to save it to
-     * @param preferences The PreferenceHierarchy class
-     * @param <P> The type of PreferenceHierarchy class
-     * @throws PreferenceWriteException If the file could not be written or serialized
+     * Tries to save the preference class as TOML in the specified location, otherwise invokes callback.
+     *
+     * @param file The location to save the preferences.
+     * @param clazz The class to deserializes and save.
+     * @param onErrorCallback Message supplier containing the error message. Can be null if logging is irrelevant.
+     * @param <T> The type of preference class.
      */
-    public static <P extends PreferenceHierarchy> void save(File file, P preferences) throws PreferenceWriteException {
-        TomlMapper mapper = createMapper();
-
+    public static <T extends PreferenceHierarchy> void savePreferenceToFileOrCallback(
+            File file,
+            T clazz,
+            @NonNull Consumer<String> onErrorCallback
+    ) {
         try {
-            mapper.writerWithDefaultPrettyPrinter().writeValue(file, preferences);
+            savePreferencesToFile(file, clazz);
         } catch (IOException e) {
-            throw new PreferenceWriteException("Failed to serialize preferences", e);
+            onErrorCallback.accept("Failed to save preferences to: " + file.getAbsolutePath());
         }
+    }
+
+    /**
+     * Loads a preference class from TOML in the specified location.
+     *
+     * @param file The file to load the TOML from.
+     * @param clazz The clazz to deserialize the TOML into.
+     * @return The deserialized preference object.
+     * @param <T> The type of preference class.
+     * @throws IOException In case something file-related goes wrong.
+     */
+    public static <T extends PreferenceHierarchy> T loadPreferencesFromFile(File file, T clazz)
+            throws IOException
+    {
+        String read = Files.readString(Path.of(file.getAbsolutePath()));
+        return PreferenceSerializer.deserializeFromToml(read, clazz);
+    }
+
+    /**
+     * Saves a preference class to a file in the specified location in the TOML format.
+     *
+     * @param file The file location to save the preference class to.
+     * @param clazz The class to serialize.
+     * @param <T> The type of preference class.
+     * @throws IOException In case something file-related goes wrong.
+     */
+    public static <T extends PreferenceHierarchy> void savePreferencesToFile(File file, T clazz)
+            throws IOException
+    {
+        String toml = PreferenceSerializer.serializeToToml(PreferenceSerializer.scan(clazz));
+        Files.write(Path.of(file.getAbsolutePath()), toml.getBytes());
     }
 
 }
