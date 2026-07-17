7e8c112ad2d69ae45b6a8dea809e4d68cef18ac8
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-07-17 17:21:41 +0200

    Refactor ClientPreferences to use generic types and codecs for preference values and new annotations

--- src/client/java/at/tobiazsh/myworld/traffic_addition/preference/ClientPreferences.java
+++ src/client/java/at/tobiazsh/myworld/traffic_addition/preference/ClientPreferences.java
@@ -1,40 +1,44 @@
 package at.tobiazsh.myworld.traffic_addition.preference;
 
-import at.tobiazsh.myworld.traffic_addition.toml.TomlFloat;
-import at.tobiazsh.myworld.traffic_addition.toml.TomlInteger;
-import at.tobiazsh.myworld.traffic_addition.toml.TomlString;
+import at.tobiazsh.myworld.traffic_addition.preference.annotation.PreferenceChild;
+import at.tobiazsh.myworld.traffic_addition.preference.annotation.PreferenceRoot;
+import at.tobiazsh.myworld.traffic_addition.preference.codec.Codecs;
 
+@PreferenceRoot
 public class ClientPreferences implements PreferenceHierarchy {
 
-    // DO NOT BLINDLY CHANGE OBJECT NAMES! It's essential for TOML!
     public final CustomizableSigns customizableSigns = new CustomizableSigns();
     public final Signs signs = new Signs();
     public final Rendering rendering = new Rendering();
     public final General general = new General();
 
+    @PreferenceChild("customizable_signs")
     public static class CustomizableSigns {
         private CustomizableSigns() {}
 
-        public final Preference<TomlFloat> elementDistancing = new Preference<>(new TomlFloat(0.75f), "view_distance");
-        public final Preference<TomlFloat> viewDistance = new Preference<>(new TomlFloat(3f), "element_distancing");
+        public final Preference<Float> elementDistancing = new Preference<>(0.75f, "view_distance", Codecs.FLOAT);
+        public final Preference<Float> viewDistance = new Preference<>(3f, "element_distancing", Codecs.FLOAT);
     }
 
+    @PreferenceChild("signs")
     public static class Signs {
         private Signs() {}
 
-        public final Preference<TomlFloat> viewDistance = new Preference<>(new TomlFloat(3f), "view_distance");
+        public final Preference<Float> viewDistance = new Preference<>(3f, "view_distance", Codecs.FLOAT);
     }
 
+    @PreferenceChild("rendering")
     public static class Rendering {
         private Rendering() {}
 
-        public final Preference<TomlInteger> imageRenderLayerCacheSize = new Preference<>(new TomlInteger(200), "image_render_layer_cache_size");
-        public final Preference<TomlInteger> textRenderLayerCacheSize = new Preference<>(new TomlInteger(100), "text_render_layer_cache_size");
+        public final Preference<Integer> imageRenderLayerCacheSize = new Preference<>(200, "image_render_layer_cache_size", Codecs.INTEGER);
+        public final Preference<Integer> textRenderLayerCacheSize = new Preference<>(100, "text_render_layer_cache_size", Codecs.INTEGER);
     }
 
+    @PreferenceChild("general")
     public static class General {
         private General() {}
 
-        public final Preference<TomlString> language = new Preference<>(new TomlString("auto"), "language");
+        public final Preference<String> language = new Preference<>("auto", "language", Codecs.STRING);
     }
 }
