0357200bcba5c4bdc3029e8ab2ffea5c46f1da0b
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-07-28 19:03:06 +0200

    Permission System [WIP]

---
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/command/PermissionCommand.java
@@ -0,0 +1,23 @@
+package at.tobiazsh.myworld.traffic_addition.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.protocol.status.ServerStatus;
+import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.NotNull;
+
+public class PermissionCommand {
+
+//    private static void registerCommand(@NotNull CommandDispatcher<CommandSourceStack> dispatcher, @NotNull String name) {
+//        dispatcher.register(Commands
+//                .literal(name)
+//                    .requires(Commands.hasPermission(Commands.LEVEL_GAMEMASTERS))
+//                    .then(Commands.argument("player", EntityArgument.players())
+//                            .then(Commands.argument("permission", ))
+//                    )
+//    }
+
+
+}
---
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/command/argument_types/MwtaPermissionsArgument.java
@@ -0,0 +1,12 @@
+package at.tobiazsh.myworld.traffic_addition.command.argument_types;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+//public class MwtaPermissionsArgument extends ArgumentType<> {
+//    @Override
+//    public Object parse(StringReader stringReader) throws CommandSyntaxException {
+//        return null;
+//    }
+//}
