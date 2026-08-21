ec0cbc1cf216902c84f487c1b59d53b53fd5e0e7
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-08-21 13:43:52 +0200

    Convert Tuple to record class

--- src/main/java/at/tobiazsh/myworld/traffic_addition/utils/Tuple.java
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/utils/Tuple.java
@@ -1,24 +1,7 @@
 package at.tobiazsh.myworld.traffic_addition.utils;
 
-public final class Tuple<A, B> {
-
-    private final A a;
-    private final B b;
-
-    public Tuple(A a, B b) {
-        this.a = a;
-        this.b = b;
-    }
-
+public record Tuple<A, B>(A a, B b) {
     public static <A, B> Tuple<A, B> of(A a, B b) {
         return new Tuple<>(a, b);
-    }
-
-    public A getA() {
-        return a;
-    }
-
-    public B getB() {
-        return b;
     }
 }
